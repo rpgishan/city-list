83f310d268f40835b03a25f87730be56bf175daf
Gishan Pemathilake <gishan.pemathilake@syscolabs.com>
2022-07-10 15:31:41 +0530

    implement API token validation

--- city-list-core-api/src/main/java/com/gish/citylist/citylistcoreapi/controller/JwtAuthenticationController.java
+++ city-list-core-api/src/main/java/com/gish/citylist/citylistcoreapi/controller/JwtAuthenticationController.java
@@ -5,6 +5,7 @@ import com.gish.citylist.citylistcoreapi.dto.JwtResponse;
 import com.gish.citylist.citylistcoreapi.service.UserDetailsServiceImpl;
 import com.gish.citylist.citylistcoreapi.util.JwtTokenUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -12,9 +13,10 @@ import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -36,7 +38,7 @@ public class JwtAuthenticationController {
         this.userDetailsService = userDetailsService;
     }
 
-    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    @PostMapping("/authenticate")
     public ResponseEntity<?> createAuthenticationToken(@RequestBody final JwtRequest authenticationRequest) throws
                                                                                                             Exception {
 
@@ -48,6 +50,11 @@ public class JwtAuthenticationController {
         final String token = jwtTokenUtil.generateToken(userDetails);
 
         return ResponseEntity.ok(new JwtResponse(token));
+    }
+
+    @GetMapping("/validate")
+    @ResponseStatus(code = HttpStatus.OK)
+    public void validateToken() {
     }
 
     private void authenticate(final String username, final String password) throws Exception {
